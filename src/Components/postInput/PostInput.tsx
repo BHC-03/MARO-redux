cafec6cd4b192a35f336d642af69411733f44d88
BHC-03 <7meeed999@gmail.com>
2022-07-10 13:10:32 +0200

    some stuff

--- src/Components/postInput/PostInput.tsx
+++ src/Components/postInput/PostInput.tsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React ,{useState}from "react";
+import useAppSelector from "../../Hooks/ReduxHooks";
+import fire from "../../firebaseConfig";
 import './styling/PostInput.css'
+import { postType } from "../../Types/Posts/posts";
 const PostInput:React.FC = ()=>{
+    const [text,setText] = useState<string>('');
+    const currentUser = useAppSelector(state=> state.user);
+
+
+
+
+
+    const clickHandler = async()=>{ 
+        const id = 'someId' //TODO:change it with the library
+        const newPost:postType = { //TODO: implement a post type
+            id,
+            author:currentUser.id,
+            text,
+            upvotes:[],
+            downvotes:[],
+            comments:[],
+            timing: Date.now(),
+        }
+        await fire.firestore().doc(`${id}`).set(newPost);
+        console.log('post added');
+    }
+    const changeHandler = (e:React.ChangeEvent<HTMLTextAreaElement>)=>{
+        setText(e.target.value);
+    }
     return (
         <div className="postInputContainer">
             <p className="textAreaLabel">What's on your mind ?</p>
-            <textarea style={{width:'70%',height:'180px'}} className="postInputArea" placeholder="Write down your story"></textarea>
+            <textarea value={text} onChange={changeHandler} style={{width:'70%',height:'180px'}} className="postInputArea" placeholder="Write down your story"></textarea>
             <div className="controlls">
-                <div className="postSubmitButton">
+                <div onClick={clickHandler} className="postSubmitButton">
                     Submit
                 </div>
             </div>
---
+++ src/Types/Posts/posts.ts
@@ -0,0 +1,10 @@
+export interface postType {
+    text:string,
+    id:string,
+    upvotes:string[],
+    downvotes:string[],
+    comments:[]//TODO:implement the comment type
+    ,author:string,
+    timing:number
+
+}
